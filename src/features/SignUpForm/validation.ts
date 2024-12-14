220f2bcdbac2057e29c24fd145475d716963c8d1
Viktor.Kondolovskyi <viktor.kondolovskyi@tui.com>
2024-12-14 16:21:16 +0200

    add domain validation in email

--- src/features/SignUpForm/validation.ts
+++ src/features/SignUpForm/validation.ts
@@ -19,6 +19,10 @@ export const passwordRules: PasswordRule[] = [
 export const validationSchema = Yup.object().shape({
     email: Yup.string()
         .email('Invalid email address')
+        .matches(
+            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            'Invalid email address'
+        )
         .required('Email is required'),
     password: Yup.string()
         .min(8, 'Password must be at least 8 characters')
